9b3165ef94533d9e1659d50d96675303ef0aa893
Fuad1610006 <afmfuad96@gmail.com>
2024-03-06 21:59:38 +0600

    tiny edit

--- resources/views/backend/about-us/index.blade.php
+++ resources/views/backend/about-us/index.blade.php
@@ -1,5 +1,16 @@
 @extends('backend.layout.app')
-@section('content')           
+@section('content') 
+<style>
+    /* Hide CKEditor specific elements */
+    .ck.ck-widget__type-around__button.ck-widget__type-around__button_before,
+    .ck.ck-widget__type-around__button.ck-widget__type-around__button_after {
+        display: none;
+    }
+    /* Additional styles to remove unwanted space */
+    .ck.ck-content > div {
+        margin-bottom: 0;
+    }
+</style>          
 <div class="page-heading">
     <div class="page-title">
         <div class="row">
